83ffcd1d5c95bb0b2554add3abdd8892263f260e
caua882730 <cavalcanteian31@gmail.com>
2025-03-27 16:11:30 -0300

    Add files via upload

---
+++ conectar_api_banco.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+    'db_host' => 'localhost', 
+    'db_user' => 'cauaj_pay',
+    'db_pass' => 'reinan16p@',
+    'db_name' => 'cauaj_pay'
+);
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-// Redireciona para o diretório /web
-header("Location: https://web.uranopay.com");
-exit;
-?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirecionando...</title>
+    <script type="text/javascript">
+        // Redirecionar para a URL desejada
+        window.location.href = "login";
+    </script>
+</head>
+<body>
+</body>
+</html>
